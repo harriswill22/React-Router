b5fc5b73694ffeb2ac944a44c8d858faa7e0f921
Will Harris <Harriswill22@yahoo.com>
2018-12-10 10:58:13 -0500

    creates components for App js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import About from './About'
 import Home from './Home'
 import Cats from './Cats'
+import NavBar from './NavBar'
 import {BrowserRouter as Router, //Watches from changes to URL
   Route,  //Renders components based on the specific URL
   Link }  // Changes the URL so the Router sees it
@@ -13,11 +14,7 @@ class App extends Component {
     return (
       <Router>
       <div className="App">
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/cats">Cats</Link></li>
-      </ul>
+      <NavBar/>
       <Route path="/" exact component={Home}/>
       <Route path="/about" component={About}/>
       <Route path="/cats" component={Cats}/>
---
+++ src/Cats.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Cats =(props) => {
+    return (
+        <div>
+            <h1>Cats</h1>
+        </div>
+    );
+};
+export default Cats;
