5282bf4b51a2492b4522d735c85c332f2fb8bcf7
Will Harris <Harriswill22@yahoo.com>
2018-12-10 12:00:10 -0500

    dynamic cat names from this.state.catToys

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
 import React, { Component } from 'react';
-import About from './About'
-import Home from './Home'
-import Cats from './Cats'
-import NavBar from './NavBar'
-import OneCat from './OneCat'
-
-import {BrowserRouter as Router, //Watches from changes to URL
-  Route,  //Renders components based on the specific URL
-  //Link 
-}  // Changes the URL so the Router sees it
-from 'react-router-dom'
+import logo from './logo.svg';
 import './App.css';
-import Link from 'react-router-dom/Link';
+
+import {
+  BrowserRouter as Router, // Watches for changes to URL
+  Route,                   // Renders components based on the specific URL
+  //Link                     // Changes the URL so the Router sees it
+} from 'react-router-dom';
+
+import NavBar from './NavBar';
+import Home from './Home';
+import About from './About';
+import Cats from './Cats';
+import OneCat from './OneCat';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // catList: [
+      //   'Oakley',
+      //   'Milla',
+      //   'Whisper',
+      //   'Neo'
+      // ],
+      catToys: {
+        'Oakley': [
+          'string',
+          'bookmarks',
+          'ice',
+          'daddy\'s face'
+        ],
+        'Milla': [
+          'drool'
+        ],
+        'Whisper': [
+          'string'
+        ],
+        'Neo': [
+          'the couch'
+        ]
+      }
+    };
+  }
   render() {
     return (
       <Router>
-      <div className="App">
-      <NavBar/>
-      <Route path="/" exact component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/cats/" component={Cats}/>
-      <Route path="/cats/:catName" component={OneCat}/>
-      </div>
-    </Router>
+        <div>
+          <NavBar />
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/cats" render={(props) => {
+            return <Cats catList={Object.keys(this.state.catToys)} {...props} />
+          }} />          
+          <Route path="/cats/:catName" render={(props) => {
+            return <OneCat toys={this.state.catToys} {...props} />
+          }} />
+        </div>
+      </Router>
     );
   }
 }
