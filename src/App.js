d2a483f6ef4210299632ea33cb20816578d4b4dc
Will Harris <Harriswill22@yahoo.com>
2018-12-10 10:31:19 -0500

    create compnents with links

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import About from './About'
+import Home from './Home'
+import Cats from './Cats'
+import {BrowserRouter as Router, //Watches from changes to URL
+  Route,  //Renders components based on the specific URL
+  Link }  // Changes the URL so the Router sees it
+from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/cats">Cats</Link></li>
+      </ul>
+      <Route path="/" exact component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/cats" component={Cats}/>
       </div>
+    </Router>
     );
   }
 }
