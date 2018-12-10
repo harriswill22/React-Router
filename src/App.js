353a0ef47215d5d1edee67791ef1f9a7b6564e5c
Will Harris <Harriswill22@yahoo.com>
2018-12-10 11:21:20 -0500

    adds params for getting one cat

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import About from './About'
 import Home from './Home'
 import Cats from './Cats'
 import NavBar from './NavBar'
+import OneCat from './OneCat'
+
 import {BrowserRouter as Router, //Watches from changes to URL
   Route,  //Renders components based on the specific URL
-  Link }  // Changes the URL so the Router sees it
+  //Link 
+}  // Changes the URL so the Router sees it
 from 'react-router-dom'
 import './App.css';
+import Link from 'react-router-dom/Link';
 
 class App extends Component {
   render() {
@@ -17,7 +21,8 @@ class App extends Component {
       <NavBar/>
       <Route path="/" exact component={Home}/>
       <Route path="/about" component={About}/>
-      <Route path="/cats" component={Cats}/>
+      <Route path="/cats/" component={Cats}/>
+      <Route path="/cats/:catName" component={OneCat}/>
       </div>
     </Router>
     );
